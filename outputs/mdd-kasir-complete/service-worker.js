8f31335fba120872b54460cc8dd46a2245ed9970
FriendsIndonesia <friendsindonesia28@gmail.com>
2026-08-31 05:55:39 +0700

    Aktifkan cache versi 70 perlindungan spreadsheet

--- outputs/mdd-kasir-complete/service-worker.js
+++ outputs/mdd-kasir-complete/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mdd-material-pro-v69-finance-checkbox-search";
+const CACHE_NAME = "mdd-material-pro-v70-sheet-edit-safety";
 const APP_SHELL = [
   "./",
   "./matrialpro.html",

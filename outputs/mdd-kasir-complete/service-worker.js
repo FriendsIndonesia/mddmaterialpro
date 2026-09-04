61187411de8f9b2245d09c1667a32c683ed727b4
FriendsIndonesia <friendsindonesia28@gmail.com>
2026-09-04 08:05:06 +0700

    Release V96 finance invoice printing cache

--- outputs/mdd-kasir-complete/service-worker.js
+++ outputs/mdd-kasir-complete/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mdd-material-pro-v95-authoritative-backend";
+const CACHE_NAME = "mdd-material-pro-v96-finance-invoice-print";
 const APP_SHELL = [
   "./",
   "./matrialpro.html",
